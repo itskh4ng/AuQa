36c9cd6c4e39a8c2ee7e05dfad4145c149f4f288
itskh4ng-edu <phamj@pdxeaglestudent.org>
2025-01-23 10:01:24 -0800

    Update gamepad.js

--- assets/gamepad.js
+++ assets/gamepad.js
@@ -2,15 +2,17 @@ function displaySelectedOption() {
    var e = document.getElementById("gamepad").value;
    var t = document.getElementById("main-iframe");
 
-   if (e === "geforce") {
+   if (e === "GeForce") {
       t.src = "../storage/geforce.html";
-   } else if (e === "now.gg") {
+   } else if (e === "Now.GG") {
       t.src = "../storage/nowgg.html";
-   } else if (e === "ngg.lol") {
+   } else if (e === "NGG.LOL") {
       t.src = "../storage/ngg.html";
-   } else if (e === "itch.io") {
+   } else if (e === "Itch.IO") {
       t.src = "../storage/itch.html";
-   } else if (e === "bloxd.io") {
+   } else if (e === "Bloxd.IO") {
       t.src = "../storage/bloxd.html";
+   } else if (e === "Retro Bowl") {
+      t.src = "https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://274079163-174037128449362058.preview.editmysite.com/uploads/b/139890129-899796311729593176/files/rtbl.xml&container=ig";
    }
 }
