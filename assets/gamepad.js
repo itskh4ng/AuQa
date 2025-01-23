08a438cc921adba7929440fdf83a6a8576cb552b
itskh4ng-edu <phamj@pdxeaglestudent.org>
2025-01-23 09:31:56 -0800

    Update gamepad.js

--- assets/gamepad.js
+++ assets/gamepad.js
@@ -2,9 +2,15 @@ function displaySelectedOption() {
    var e = document.getElementById("gamepad").value;
    var t = document.getElementById("main-iframe");
 
-   e === "geforce" ? t.src = "../storage/geforce.html" : 
-   e === "now.gg" ? t.src = "../storage/nowgg.html" : 
-   e === "ngg.lol" ? t.src = "../storage/ngg.html" : 
-   e === "itch.io" ? t.src = "../storage/itch.html" : 
-   e === "bloxd.io" && (t.src = "https://bloxd.io");
+   if (e === "geforce") {
+      t.src = "../storage/geforce.html";
+   } else if (e === "now.gg") {
+      t.src = "../storage/nowgg.html";
+   } else if (e === "ngg.lol") {
+      t.src = "../storage/ngg.html";
+   } else if (e === "itch.io") {
+      t.src = "../storage/itch.html";
+   } else if (e === "bloxd.io") {
+      t.src = "https://bloxd.io";
+   }
 }
