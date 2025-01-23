02267fb57c7db46c4856f176a468b7882240fb61
itskh4ng <itskh4ng@gmail.com>
2025-01-23 07:56:20 -0800

    Update vencord.js

--- assets/vencord.js
+++ assets/vencord.js
@@ -1,27 +1,19 @@
 function toggleVencord() {
-  // Get the iframe element by ID
   const iframe = document.getElementById('main-iframe');
-  
-  // Ensure the iframe's document is accessible
   const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
 
-  // Check if the script is already loaded in the iframe
   if (!iframeDoc.getElementById('vencord-script')) {
-    // Create a new script element for the iframe
     const script = iframeDoc.createElement('script');
-    script.id = 'vencord-script';  // Unique ID for the script element
+    script.id = 'vencord-script';
     script.src = 'https://raw.githubusercontent.com/Vencord/builds/main/Vencord.user.js';
     script.onload = function() {
-      console.log('Vencord script loaded and executed inside the iframe!');
-    };
+      console.log('Success.');
+    }
     script.onerror = function() {
-      console.error('Error loading the Vencord script inside the iframe.');
+      console.error('Error.');
     };
-
-    // Append the script to the iframe document
     iframeDoc.body.appendChild(script);
   } else {
-    console.log('Vencord script is already loaded inside the iframe.');
+    console.log('Vencord Loaded.');
   }
 }
-
