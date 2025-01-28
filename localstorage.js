3df927b18ff3ad31a4db1eab4f249d5af6209d24
itskh4ng-edu <phamj@pdxeaglestudent.org>
2025-01-28 09:55:29 -0800

    Update localstorage.js

--- localstorage.js
+++ localstorage.js
@@ -1,43 +1,40 @@
-function displaySelectedOption() {
-    var e = document.getElementById("gamepad").value;
-    var t = document.getElementById("main-iframe");
-    
+function saveSelectedOption(e) {
     localStorage.setItem("selectedGame", e);
-    
-    if (e === "GeForce") {
-        t.src = "../storage/geforce.html";
-    } else if (e === "Now.GG") {
-        t.src = "../storage/nowgg.html";
-    } else if (e === "EASYFUN") {
-        t.src = "../storage/easyfun.html";
-    } else if (e === "Itch.IO") {
-        t.src = "../storage/itch.html";
-    } else if (e === "1v1.lol") {
-        t.src = "https://1v1.lol";
-    } else if (e === "CrazyGames") {
-        t.src = "../storage/crazygames.html";
-    } else if (e === "RetroBowl") {
-        t.src = "https://game316009.konggames.com/gamez/0031/6009/live/index.html";
-    } else if (e === "CookieClicker") {
-        t.src = "../storage/cookieclicker.html";
-    } else if (e === "SubwaySurfers") {
-        t.src = "../storage/subwaysurfers.html";
-    } else if (e === "FruitNinja") {
-        t.src = "../storage/fruitninja.html";
-    }
 }
 
 function loadSelectedOption() {
     var e = localStorage.getItem("selectedGame");
-    var t = document.getElementById("gamepad");
-    var r = document.getElementById("main-iframe");
-    
     if (e) {
+        var t = document.getElementById("gamepad");
+        var r = document.getElementById("main-iframe");
         t.value = e;
-        displaySelectedOption();
-    } else {
-        r.src = "";
+        if (e === "GeForce") {
+            r.src = "../storage/geforce.html";
+        } else if (e === "Now.GG") {
+            r.src = "../storage/nowgg.html";
+        } else if (e === "EASYFUN") {
+            r.src = "../storage/easyfun.html";
+        } else if (e === "Itch.IO") {
+            r.src = "../storage/itch.html";
+        } else if (e === "1v1.lol") {
+            r.src = "https://1v1.lol";
+        } else if (e === "CrazyGames") {
+            r.src = "../storage/crazygames.html";
+        } else if (e === "RetroBowl") {
+            r.src = "https://game316009.konggames.com/gamez/0031/6009/live/index.html";
+        } else if (e === "CookieClicker") {
+            r.src = "../storage/cookieclicker.html";
+        } else if (e === "SubwaySurfers") {
+            r.src = "../storage/subwaysurfers.html";
+        } else if (e === "FruitNinja") {
+            r.src = "../storage/fruitninja.html";
+        }
     }
 }
 
-window.onload = loadSelectedOption;
+window.onload = function() {
+    loadSelectedOption();
+    setInterval(function() {
+        saveSelectedOption(document.getElementById("gamepad").value);
+    }, 10);
+};
