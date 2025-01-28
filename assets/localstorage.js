0a13f7c5a959e1e66eeb1dc0dc7c501b6b2b901c
itskh4ng-edu <phamj@pdxeaglestudent.org>
2025-01-28 11:05:55 -0800

    Update localstorage.js

--- assets/localstorage.js
+++ assets/localstorage.js
@@ -17,32 +17,24 @@ function getCookie(c) {
 function saveIframeSrc() {
     var iframe = document.getElementById("main-iframe");
     if (iframe && iframe.contentWindow) {
-        console.log("Saving iframe src: " + iframe.src);  // Debug log
         setCookie("iframeSrc", iframe.src, 7);
-    } else {
-        console.log("Iframe not found or no contentWindow.");  // Debug log
     }
 }
 
 function loadIframeSrc() {
     var iframeSrc = getCookie("iframeSrc");
-    console.log("Loading iframe src: " + iframeSrc);  // Debug log
     if (iframeSrc) {
         var iframe = document.getElementById("main-iframe");
-        if (iframe) {
-            iframe.src = iframeSrc; 
-        }
+        if (iframe) iframe.src = iframeSrc;
     }
 }
 
 window.onload = function() {
     loadIframeSrc();
-    
-    // Ensure iframe is loaded before saving its src
     var iframe = document.getElementById("main-iframe");
     if (iframe) {
         iframe.onload = function() {
-            setInterval(saveIframeSrc, 10);  
+            setInterval(saveIframeSrc, 100);
         };
     }
 };
