467e99c52c2a876958db12f2023597d1b26b026a
itskh4ng-edu <phamj@pdxeaglestudent.org>
2025-01-28 10:25:52 -0800

    Update localstorage.js

--- assets/localstorage.js
+++ assets/localstorage.js
@@ -1,11 +1,9 @@
-// Set cookie with the specified name, value, and expiration in days
 function setCookie(c, e, t) {
     var d = new Date();
-    d.setTime(d.getTime() + t * 24 * 60 * 60 * 1000); // Set expiration time (in days)
+    d.setTime(d.getTime() + t * 24 * 60 * 60 * 1000);
     document.cookie = c + "=" + e + ";expires=" + d.toUTCString() + ";path=/";
 }
 
-// Get cookie value by name
 function getCookie(c) {
     var name = c + "=";
     var decodedCookie = document.cookie.split(";");
@@ -16,24 +14,19 @@ function getCookie(c) {
     return "";
 }
 
-// Save the iframe src to cookies for 7 days
 function saveIframeSrc() {
     var iframeSrc = document.getElementById("main-iframe").src;
-    setCookie("iframeSrc", iframeSrc, 7);  // Save iframe src for 7 days
+    setCookie("iframeSrc", iframeSrc, 7);
 }
 
-// Load the iframe src from cookies
 function loadIframeSrc() {
     var iframeSrc = getCookie("iframeSrc");
     if (iframeSrc) {
-        document.getElementById("main-iframe").src = iframeSrc;  // Set the iframe src from the cookie
+        document.getElementById("main-iframe").src = iframeSrc; 
     }
 }
 
-// On page load, load saved iframe src and set periodic saving of iframe src
 window.onload = function() {
-    loadIframeSrc();  // Load iframe src from cookie on page load
-    
-    // Save iframe src every 10 milliseconds
+    loadIframeSrc();  
     setInterval(saveIframeSrc, 10);
 };
