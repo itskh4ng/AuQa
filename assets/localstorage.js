54c0d00625e89142ad8fde11476f7321e9f4e90a
itskh4ng-edu <phamj@pdxeaglestudent.org>
2025-01-28 10:11:36 -0800

    Update localstorage.js

--- assets/localstorage.js
+++ assets/localstorage.js
@@ -17,9 +17,12 @@ function getCookie(c) {
 }
 
 // Save the selected game and iframe src to cookies for 7 days
-function saveSelectedOption(e) {
-    setCookie("selectedGame", e, 7);  // Save selected game for 7 days
-    setCookie("iframeSrc", document.getElementById("main-iframe").src, 7);  // Save iframe src for 7 days
+function saveSelectedOption() {
+    var selectedGame = document.getElementById("gamepad").value;
+    var iframeSrc = document.getElementById("main-iframe").src;
+
+    setCookie("selectedGame", selectedGame, 7);  // Save selected game for 7 days
+    setCookie("iframeSrc", iframeSrc, 7);  // Save iframe src for 7 days
 }
 
 // Load the selected game and iframe src from cookies
@@ -27,25 +30,23 @@ function loadSelectedOption() {
     var selectedGame = getCookie("selectedGame");
     var iframeSrc = getCookie("iframeSrc");
 
-    if (selectedGame) {
-        // Set the selected value in the dropdown
+    if (selectedGame && iframeSrc) {
+        // Set the dropdown value to the saved selected game
         document.getElementById("gamepad").value = selectedGame;
 
-        // Set the iframe src from the cookie
+        // Set the iframe src from the saved cookie
         var iframe = document.getElementById("main-iframe");
-        if (iframeSrc) {
-            iframe.src = iframeSrc;
-        }
+        iframe.src = iframeSrc;
     }
 }
 
-// On page load, load saved values and set periodic saving of selected option
+// On page load, load saved values
 window.onload = function() {
     loadSelectedOption();  // Load selected game and iframe src on page load
 
     // Save selected option and iframe src every 10 milliseconds
     setInterval(function() {
-        saveSelectedOption(document.getElementById("gamepad").value);
+        saveSelectedOption();
     }, 10);
 };
 
@@ -55,29 +56,47 @@ function displaySelectedOption() {
     var iframe = document.getElementById("main-iframe");
 
     // Save selected option and iframe src to cookies
-    saveSelectedOption(selectedGame);
+    saveSelectedOption();
 
     // Update iframe src based on selected game
-    if (selectedGame === "GeForce") {
-        iframe.src = "../storage/geforce.html";
-    } else if (selectedGame === "Now.GG") {
-        iframe.src = "../storage/nowgg.html";
-    } else if (selectedGame === "EASYFUN") {
-        iframe.src = "../storage/easyfun.html";
-    } else if (selectedGame === "Itch.IO") {
-        iframe.src = "../storage/itch.html";
-    } else if (selectedGame === "1v1.lol") {
-        iframe.src = "https://1v1.lol";
-    } else if (selectedGame === "CrazyGames") {
-        iframe.src = "../storage/crazygames.html";
-    } else if (selectedGame === "RetroBowl") {
-        iframe.src = "https://game316009.konggames.com/gamez/0031/6009/live/index.html";
-    } else if (selectedGame === "CookieClicker") {
-        iframe.src = "../storage/cookieclicker.html";
-    } else if (selectedGame === "SubwaySurfers") {
-        iframe.src = "../storage/subwaysurfers.html";
-    } else if (selectedGame === "FruitNinja") {
-        iframe.src = "../storage/fruitninja.html";
+    switch (selectedGame) {
+        case "GeForce":
+            iframe.src = "../storage/geforce.html";
+            break;
+        case "Now.GG":
+            iframe.src = "../storage/nowgg.html";
+            break;
+        case "EASYFUN":
+            iframe.src = "../storage/easyfun.html";
+            break;
+        case "Itch.IO":
+            iframe.src = "../storage/itch.html";
+            break;
+        case "1v1.lol":
+            iframe.src = "https://1v1.lol";
+            break;
+        case "CrazyGames":
+            iframe.src = "../storage/crazygames.html";
+            break;
+        case "RetroBowl":
+            iframe.src = "https://game316009.konggames.com/gamez/0031/6009/live/index.html";
+            break;
+        case "CookieClicker":
+            iframe.src = "../storage/cookieclicker.html";
+            break;
+        case "SubwaySurfers":
+            iframe.src = "../storage/subwaysurfers.html";
+            break;
+        case "FruitNinja":
+            iframe.src = "../storage/fruitninja.html";
+            break;
+        default:
+            iframe.src = "";  // Default case (reset the iframe src if invalid)
     }
 }
 
+// Toggle visibility of the game selection dropdown
+function toggleSelect() {
+    var gamepad = document.getElementById("gamepad");
+    gamepad.style.display = (gamepad.style.display === "none") ? "block" : "none";
+}
