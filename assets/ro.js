aacd80038f58c6801e222fbdb14b09860b3130cb
itskh4ng <itskh4ng@gmail.com>
2025-02-05 20:21:23 -0800

    Update ro.js

--- assets/ro.js
+++ assets/ro.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (iframe.contentWindow) {
                 let videos = iframe.contentWindow.document.querySelectorAll("video, source");
                 videos.forEach(video => {
-                    if (video.src.includes("ngg.lol/apps/frogiee1/videos/loading.mp4")) {
+                    if (video.src.includes("hvtr%3A-%2Flge.non%2Fcprs-fpoeige3%2Ftifems-lmafilg%2Cmr4")) {
                         video.remove();
                         console.log("Blocked: ngg.lol/apps/frogiee1/videos/loading.mp4");
                     }
