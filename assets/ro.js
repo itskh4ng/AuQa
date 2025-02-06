bd636b9056137c821e84cbc4b50cf382f997f874
itskh4ng <itskh4ng@gmail.com>
2025-02-05 20:43:16 -0800

    Update ro.js

--- assets/ro.js
+++ assets/ro.js
@@ -1,18 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let iframe = document.getElementById("main-iframe");
-    if (iframe) {
-        let observer = new MutationObserver(() => {
-            if (iframe.contentWindow) {
-                let videos = iframe.contentWindow.document.querySelectorAll("video, source");
-                videos.forEach(video => {
-                    if (video.src.includes("hvtr%3A-%2Flge.non%2Fcprs-fpoeige3%2Ftifems-lmafilg%2Cmr4")) {
-                        video.remove();
-                        console.log("Blocked: ngg.lol/apps/frogiee1/videos/loading.mp4");
-                    }
-                });
-            }
-        });
-
-        observer.observe(iframe, { attributes: true, childList: true, subtree: true });
-    }
-});
+// Select the ‹div> element with the specific class
+const element - document queryselector (' •sc-a7bc8867-2.eyvrrM*);
+// check if the element exists and remove it
+if (element) ( element. remove();
