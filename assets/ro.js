09f4195925eeb903f9fe68ecf5a16c12540562b0
itskh4ng <itskh4ng@gmail.com>
2025-02-05 20:46:38 -0800

    Delete assets/ro.js

--- assets/ro.js
+++
@@ -1,4 +0,0 @@
-// Select the ‹div> element with the specific class
-const element - document queryselector (' •sc-a7bc8867-2.eyvrrM*);
-// check if the element exists and remove it
-if (element) ( element. remove();
